a8cd206e55b0a082d0197f0162216e4553697583
GennaBari <barinovaevgenia84@gmail.com>
2024-07-20 09:39:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+first = 123
+second = 456
+third = 789
+if first == second == third:
+    print('Вывод: 3')
+elif first == second or first == third or second == third:
+    print('Вывод: 2')
+else:
+    print('Вывод: 0')
+
+first = 42
+second = 69
+third = 42
+if first == second == third:
+    print('Вывод: 3')
+elif first == second or first == third or second == third:
+    print('Вывод: 2')
+else:
+    print('Вывод: 0')
